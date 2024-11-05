3d774866097c2568db43ff50d3c7d4aa0ee7b6f3
akskwks <a95121587@gmail.com>
2024-11-05 18:19:50 +0900

    first commit

---
+++ src/main/java/com/board/back/BoardBack2Application.java
@@ -0,0 +1,13 @@
+package com.board.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBack2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardBack2Application.class, args);
+	}
+
+}
